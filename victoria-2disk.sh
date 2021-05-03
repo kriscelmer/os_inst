352321078829475550e1f7d6eddf2c9a2239b070
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-03 17:02:12 +0200

    Update and rename victoria-lvm.sh to victoria-2disk.sh

--- victoria-2disk.sh
+++ victoria-2disk.sh
@@ -1,20 +1,11 @@
 #! /bin/bash
 set -x
-cat << EOF | gdisk /dev/xvdb
-n
 
-
-
-8e00
-w
-y
-EOF
-pvcreate /dev/xvdb1 && \
-vgcreate cinder-volumes /dev/xvdb1 && \
 git clone https://opendev.org/openstack/openstack-ansible /opt/openstack-ansible && \
 cd /opt/openstack-ansible && \
 git checkout stable/victoria && \
 scripts/bootstrap-ansible.sh && \
+export BOOTSTRAP_OPTS="bootstrap_host_data_disk_device=xvdb" && \
 scripts/bootstrap-aio.sh && \
 cd playbooks && \
 openstack-ansible setup-hosts.yml && \
