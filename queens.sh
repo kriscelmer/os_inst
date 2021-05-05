38f87e47b0b5370dfe0befe8922716dc902c9516
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-05 09:47:58 +0200

    Create queens.sh

---
+++ queens.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+set -x
+git clone https://opendev.org/openstack/openstack-ansible /opt/openstack-ansible && \
+cd /opt/openstack-ansible && \
+git checkout stable/queens && \
+scripts/bootstrap-ansible.sh && \
+scripts/bootstrap-aio.sh && \
+cd playbooks && \
+openstack-ansible setup-hosts.yml && \
+openstack-ansible setup-infrastructure.yml && \
+openstack-ansible setup-openstack.yml
