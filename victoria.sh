e5ae3b9f276da2693491d3cde2a091cffcdd4992
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-11 20:37:17 +0200

    Update victoria.sh

--- victoria.sh
+++ victoria.sh
@@ -4,7 +4,7 @@ git clone https://opendev.org/openstack/openstack-ansible /opt/openstack-ansible
 cd /opt/openstack-ansible && \
 git checkout stable/victoria && \
 scripts/bootstrap-ansible.sh && \
-export SCENARIO="aio_lxc_heat_octavia" && \
+export SCENARIO="aio_metal_horizon_heat" && \
 scripts/bootstrap-aio.sh && \
 cd playbooks && \
 openstack-ansible setup-hosts.yml && \
@@ -12,3 +12,4 @@ openstack-ansible setup-infrastructure.yml && \
 openstack-ansible setup-openstack.yml && \
 echo "Password for admin:" && \
 grep keystone_auth_admin_password /etc/openstack_deploy/user_secrets.yml
+echo "REMINDER: installed scenario " $SCENARIO
