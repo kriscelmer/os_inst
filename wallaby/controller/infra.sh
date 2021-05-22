34b879b5636a047e978b1d9b283ee92cc43871e9
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-22 12:56:58 +0200

    Update infra.sh

--- wallaby/controller/infra.sh
+++ wallaby/controller/infra.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-set +x
+
 echo "----> Installing OpenStack infrastructure services"
-apt install -y chrony && \
+set -x
+apt install -qq -y chrony && \
 echo "allow 10.0.0.0/24" >> /etc/chrony/chrony.conf && \
 service chrony restart && \
 add-apt-repository -y cloud-archive:wallaby && \
-apt install -y python3-openstackclient && \
-apt install -y mariadb-server python3-pymysql && \
+apt install -qq -y python3-openstackclient && \
+apt install -qq -y mariadb-server python3-pymysql && \
 cat << EOF >  /etc/mysql/mariadb.conf.d/99-openstack.cnf
 [mysqld]
 bind-address = 10.0.0.11
@@ -26,13 +27,13 @@ y
 y
 y
 EOF
-apt install -y rabbitmq-server && \
+apt install -qq -y rabbitmq-server && \
 rabbitmqctl add_user openstack openstack && \
 rabbitmqctl set_permissions openstack ".*" ".*" ".*" && \
-apt install -y memcached python3-memcache && \
+apt install -qq -y memcached python3-memcache && \
 sed -i 's/-l 127.0.0.1/-l 10.0.0.11/' /etc/memcached.conf && \
 service memcached restart && \
-apt install -y etcd && \
+apt install -qq -y etcd && \
 cat << EOF >>  /etc/default/etcd
 ETCD_NAME="controller"
 ETCD_DATA_DIR="/var/lib/etcd"
