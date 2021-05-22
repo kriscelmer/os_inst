e56fe91be42dbc2a43d22389e4d97e29891df861
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-22 14:24:53 +0200

    Create placement.sh

---
+++ wallaby/controller/placement.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+echo "---> Installing placement"
+set -e
+set -x
+cat << EOF | mysql
+CREATE DATABASE placement;
+GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'localhost' IDENTIFIED BY 'openstack';
+GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'%' IDENTIFIED BY 'openstack';
+EOF
+. admin-openrc
+openstack user create --domain default --password openstack placement
+openstack role add --project service --user placement admin
+openstack service create --name placement --description "Placement API" placement
+openstack endpoint create --region RegionOne placement public http://controller:8778
+openstack endpoint create --region RegionOne placement internal http://controller:8778
+openstack endpoint create --region RegionOne placement admin http://controller:8778
+apt install -y placement-api python3-osc-placement> /dev/null
+crudini --set /etc/placement/placement.conf placement_database connection 'mysql+pymysql://placement:openstack@controller/placement'
+crudini --set /etc/placement/placement.conf api auth_strategy keystone
+crudini --set /etc/placement/placement.conf keystone_authtoken auth_url http://controller:5000/v3
+crudini --set /etc/placement/placement.conf keystone_authtoken memcached_servers controller:11211
+crudini --set /etc/placement/placement.conf keystone_authtoken auth_type password
+crudini --set /etc/placement/placement.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/placement/placement.conf keystone_authtoken user_domain_name Default
+crudini --set /etc/placement/placement.conf keystone_authtoken project_name service
+crudini --set /etc/placement/placement.conf keystone_authtoken username placement
+crudini --set /etc/placement/placement.conf keystone_authtoken password openstack
+su -s /bin/sh -c "placement-manage db sync" placement
+service apache2 restart
+openstack --os-placement-api-version 1.2 resource class list --sort-column name
+set +x
+echo "---> Placement installed"
