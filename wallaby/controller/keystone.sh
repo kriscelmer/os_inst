a32104867e6c92833ee3cbfad33b047163a36126
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-22 12:40:32 +0200

    Update keystone.sh

--- wallaby/controller/keystone.sh
+++ wallaby/controller/keystone.sh
@@ -1,24 +1,21 @@
 #! /bin/bash
 echo "---> Installing Keystone"
+set -e
 set -x
 cat << EOF | mysql
 CREATE DATABASE keystone;
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'openstack';
 GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'openstack';
 EOF
-apt install -y -q keystone crudini && \
-crudini --set /etc/keystone/keystone.conf database connection "mysql+pymysql://keystone:openstack@controller/keystone" && \
-crudini --set /etc/keystone/keystone.conf token provider fernet && \
-su -s /bin/sh -c "keystone-manage db_sync" keystone && \
-keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone && \
-keystone-manage credential_setup --keystone-user keystone --keystone-group keystone && \
-keystone-manage bootstrap --bootstrap-password openstack \
-  --bootstrap-admin-url http://controller:5000/v3/ \
-  --bootstrap-internal-url http://controller:5000/v3/ \
-  --bootstrap-public-url http://controller:5000/v3/ \
-  --bootstrap-region-id RegionOne && \
-echo "ServerName controller" >> /etc/apache2/apache2.conf && \
-service apache2 restart && \
+apt install -y -q keystone crudini
+crudini --set /etc/keystone/keystone.conf database connection "mysql+pymysql://keystone:openstack@controller/keystone"
+crudini --set /etc/keystone/keystone.conf token provider fernet
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap --bootstrap-password openstack --bootstrap-admin-url http://controller:5000/v3/ --bootstrap-internal-url http://controller:5000/v3/ --bootstrap-public-url http://controller:5000/v3/ --bootstrap-region-id RegionOne
+echo "ServerName controller" >> /etc/apache2/apache2.conf
+service apache2 restart
 cat << EOF > admin-openrc
 export OS_USERNAME=admin
 export OS_PASSWORD=openstack
