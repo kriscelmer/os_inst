b8d4b4b042002764b9c343219a53186692f19b00
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-22 13:10:46 +0200

    Update networking.sh

--- wallaby/controller/networking.sh
+++ wallaby/controller/networking.sh
@@ -14,5 +14,16 @@ systemctl mask systemd-networkd.socket systemd-networkd networkd-dispatcher syst
 apt -qq --assume-yes purge nplan netplan.io && \
 echo "DNS=8.8.8.8 8.8.4.4" >> /etc/systemd/resolved.conf && \
 systemctl restart systemd-resolved && \
+cat << EOF > /etc/hosts
+127.0.0.1 localhost
+10.0.0.11 controller
+10.0.0.31 compute1
+10.0.0.32 compute2
+10.0.0.33 compute3
+10.0.0.41 block1
+10.0.0.42 block2
+10.0.0.51 object1
+10.0.0.52 object2
+EOF
 set +x
 echo "---> Networking configured"
