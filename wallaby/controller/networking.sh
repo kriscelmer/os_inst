1fcdd5a7bd399631af23d60416452b3804103b1d
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-22 12:55:47 +0200

    Update networking.sh

--- wallaby/controller/networking.sh
+++ wallaby/controller/networking.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 echo "---> Connfiguring Networking for OpenStack"
-set +x
-apt install -y ifupdown && \
+set -x
+apt install -qq -y ifupdown && \
 cp os_inst/wallaby/controller/interfaces /etc/network/interfaces && \
 ifdown --force enp0s3 enp0s8 enp0s9 lo && ifup -a && \
 systemctl unmask networking && \
@@ -11,8 +11,8 @@ systemctl restart networking && \
 systemctl stop systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online && \
 systemctl disable systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online && \
 systemctl mask systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online && \
-apt-get --assume-yes purge nplan netplan.io && \
+apt -qq --assume-yes purge nplan netplan.io && \
 echo "DNS=8.8.8.8 8.8.4.4" >> /etc/systemd/resolved.conf && \
 systemctl restart systemd-resolved && \
-set -x
+set +x
 echo "---> Networking configured"
