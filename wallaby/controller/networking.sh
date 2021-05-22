c09133a45bbb15792ff46e71c9a3664ae2455fce
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-22 13:37:15 +0200

    Update networking.sh

--- wallaby/controller/networking.sh
+++ wallaby/controller/networking.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
 echo "---> Connfiguring Networking for OpenStack"
-export DEBIAN_FRONTEND=noninteractive
+set -e
 set -x
-apt -qq install ifupdown && \
-cp os_inst/wallaby/controller/interfaces /etc/network/interfaces && \
-ifdown --force enp0s3 enp0s8 enp0s9 lo && ifup -a && \
-systemctl unmask networking && \
-systemctl enable networking && \
-systemctl restart networking && \
-systemctl stop systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online && \
-systemctl disable systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online && \
-systemctl mask systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online && \
-apt -qq --assume-yes purge nplan netplan.io && \
-echo "DNS=8.8.8.8 8.8.4.4" >> /etc/systemd/resolved.conf && \
-systemctl restart systemd-resolved && \
+apt -y install ifupdown > /dev/null
+cp os_inst/wallaby/controller/interfaces /etc/network/interfaces
+ifdown --force enp0s3 enp0s8 enp0s9 lo && ifup -a
+systemctl unmask networking
+systemctl enable networking
+systemctl restart networking
+systemctl stop systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online
+systemctl disable systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online
+systemctl mask systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online
+apt -y --assume-yes purge nplan netplan.io > /dev/null
+echo "DNS=8.8.8.8 8.8.4.4" >> /etc/systemd/resolved.conf
+systemctl restart systemd-resolved
 cat << EOF > /etc/hosts
 127.0.0.1 localhost
 10.0.0.11 controller
