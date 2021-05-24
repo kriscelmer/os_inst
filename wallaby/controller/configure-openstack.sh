7235b3f70a96fbeae23e930a3f67d14ef4029ce1
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-24 17:35:00 +0200

    Update configure-openstack.sh

--- wallaby/controller/configure-openstack.sh
+++ wallaby/controller/configure-openstack.sh
@@ -3,6 +3,7 @@
 echo "---> Configuring OpenStack"
 set -e
 set -x
+. admin-openrc
 openstack network create  --share --external --provider-physical-network provider --provider-network-type flat provider
 openstack subnet create --network provider --allocation-pool start=203.0.113.101,end=203.0.113.250 --dns-nameserver 8.8.4.4 --gateway 203.0.113.1 --subnet-range 203.0.113.0/24 provider
 openstack network create selfservice
