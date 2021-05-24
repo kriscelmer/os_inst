18c5dc995269224ec059f599dcbe4dc9f320b9ee
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-24 17:05:29 +0200

    Update 2-infra.sh

--- wallaby/block1/2-infra.sh
+++ wallaby/block1/2-infra.sh
@@ -2,7 +2,8 @@ echo "----> Installing OpenStack infrastructure services"
 set -e
 set -x
 apt install -y chrony > /dev/null
-echo "allow 10.0.0.0/24" >> /etc/chrony/chrony.conf
+sed -i 's/^pool\ /#pool\ /g' /etc/chrony/chrony.conf
+echo "server controller iburst" >> /etc/chrony/chrony.conf
 service chrony restart
 add-apt-repository -y cloud-archive:wallaby > /dev/null
 set +x
