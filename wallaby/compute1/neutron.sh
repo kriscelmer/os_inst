f82502754a6603d5899dad0ac13f44b0dd13a53d
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-24 13:14:19 +0200

    Create neutron.sh

---
+++ wallaby/compute1/neutron.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "---> Installing neutron on compute1"
+set -e
+set -x
+apt install -y neutron-linuxbridge-agent crudini > /dev/null
+crudini --del /etc/neutron/neutron.conf database connection
+crudini --set /etc/neutron/neutron.conf DEFAULT transport_url 'rabbit://openstack:openstack@controller'
+crudini --set /etc/neutron/neutron.conf DEFAULT auth_strategy keystone
+crudini --set /etc/neutron/neutron.conf keystone_authtoken www_authenticate_uri http://controller:5000
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_url http://controller:5000
+crudini --set /etc/neutron/neutron.conf keystone_authtoken memcached_servers controller:11211
+crudini --set /etc/neutron/neutron.conf keystone_authtoken auth_type password
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_domain_name default
+crudini --set /etc/neutron/neutron.conf keystone_authtoken user_domain_name default
+crudini --set /etc/neutron/neutron.conf keystone_authtoken project_name service
+crudini --set /etc/neutron/neutron.conf keystone_authtoken username neutron
+crudini --set /etc/neutron/neutron.conf keystone_authtoken password openstack
+crudini --set /etc/neutron/neutron.conf oslo_concurrency lock_path /var/lib/neutron/tmp
