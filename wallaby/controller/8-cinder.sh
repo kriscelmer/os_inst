630b9fa54afa9b5d77671bda6d53130aad8b3543
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-25 19:17:41 +0200

    Update 8-cinder.sh

--- wallaby/controller/8-cinder.sh
+++ wallaby/controller/8-cinder.sh
@@ -14,12 +14,12 @@ openstack user create --domain default --password openstack cinder
 openstack role add --project service --user cinder admin
 openstack service create --name cinderv2 --description "OpenStack Block Storage" volumev2
 openstack service create --name cinderv3 --description "OpenStack Block Storage" volumev3
-openstack endpoint create --region RegionOne volumev2 public http://controller:8776/v2/%\(project_id\)s
-openstack endpoint create --region RegionOne volumev2 internal http://controller:8776/v2/%\(project_id\)s
-openstack endpoint create --region RegionOne volumev2 admin http://controller:8776/v2/%\(project_id\)s
-openstack endpoint create --region RegionOne volumev3 public http://controller:8776/v3/%\(project_id\)s
-openstack endpoint create --region RegionOne volumev3 internal http://controller:8776/v3/%\(project_id\)s
-openstack endpoint create --region RegionOne volumev3 admin http://controller:8776/v3/%\(project_id\)s
+openstack endpoint create --region RegionOne volumev2 public http://controller:8776/v2
+openstack endpoint create --region RegionOne volumev2 internal http://controller:8776/v2
+openstack endpoint create --region RegionOne volumev2 admin http://controller:8776/v2
+openstack endpoint create --region RegionOne volumev3 public http://controller:8776/v3
+openstack endpoint create --region RegionOne volumev3 internal http://controller:8776/v3
+openstack endpoint create --region RegionOne volumev3 admin http://controller:8776/v3
 apt install -y cinder-api cinder-scheduler > /dev/null
 crudini --set /etc/cinder/cinder.conf database connection 'mysql+pymysql://cinder:openstack@controller/cinder'
 crudini --set /etc/cinder/cinder.conf DEFAULT transport_url 'rabbit://openstack:openstack@controller:5672/'
