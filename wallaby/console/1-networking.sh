57a2ec9a9421163e62009853c470dafcf0dad383
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-28 09:12:18 +0200

    Update 1-networking.sh

--- wallaby/console/1-networking.sh
+++ wallaby/console/1-networking.sh
@@ -7,7 +7,7 @@ apt update > /dev/null
 apt -y dist-upgrade > /dev/null
 echo "openstack ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 apt -y install ifupdown > /dev/null
-cp os_inst/wallaby/controller/interfaces /etc/network/interfaces
+cp os_inst/wallaby/console/interfaces /etc/network/interfaces
 ifdown --force enp0s3 enp0s8 enp0s9 lo && ifup -a
 systemctl unmask networking
 systemctl enable networking
