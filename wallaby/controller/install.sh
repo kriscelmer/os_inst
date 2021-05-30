0ec0c28bb74980a2083cbf17cfdc6602237438aa
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-05-30 10:33:31 +0200

    Update install.sh

--- wallaby/controller/install.sh
+++ wallaby/controller/install.sh
@@ -3,8 +3,10 @@
 set -x
 set -e
 
-for script in os_inst/wallaby/controller/[2-9]*
+for script in os_inst/wallaby/controller/[2-9]-*
 do
   bash $script
 done
+bash os_inst/wallaby/controller/10-heat.sh
+bash os_inst/wallaby/controller/11-horizon.sh
 bash os_inst/wallaby/controller/configure-openstack.sh
