06933462292357aeb19dd9c6901e59fbd069bd88
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-06-21 20:56:24 +0200

    Update 1-networking.sh

--- wallaby/compute1/1-networking.sh
+++ wallaby/compute1/1-networking.sh
@@ -3,10 +3,10 @@
 echo "---> Configuring Networking for OpenStack"
 set -e
 set -x
-apt update > /dev/null
-apt -y dist-upgrade > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade > /dev/null
 echo "openstack ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-apt -y install ifupdown > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get -y install ifupdown > /dev/null
 cp os_inst/wallaby/compute1/interfaces /etc/network/interfaces
 ifdown --force enp0s3 enp0s8 enp0s9 lo && ifup -a
 systemctl unmask networking
@@ -15,7 +15,7 @@ systemctl restart networking
 systemctl stop systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online
 systemctl disable systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online
 systemctl mask systemd-networkd.socket systemd-networkd networkd-dispatcher systemd-networkd-wait-online
-apt -y --assume-yes purge nplan netplan.io > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get -y --assume-yes purge nplan netplan.io > /dev/null
 echo "DNS=8.8.8.8 8.8.4.4" >> /etc/systemd/resolved.conf
 systemctl restart systemd-resolved
 cat << EOF > /etc/hosts
