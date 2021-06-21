100671600783912543a41c1b9a3e4d9dd1e21d06
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-06-21 13:38:16 +0200

    Update install.sh

--- wallaby/controller/install.sh
+++ wallaby/controller/install.sh
@@ -8,6 +8,7 @@ do
   bash $script
 done
 bash os_inst/wallaby/controller/9-cinder.sh
-bash os_inst/wallaby/controller/10-heat.sh
+#bash os_inst/wallaby/controller/10-heat.sh
 bash os_inst/wallaby/controller/11-horizon.sh
 bash os_inst/wallaby/controller/configure-openstack.sh
+echo "Heat not installed!!!"
