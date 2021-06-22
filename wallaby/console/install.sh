df3c5d795f567bf11853e53fe03d8b03045a070d
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-06-22 20:56:57 +0200

    Update install.sh

--- wallaby/console/install.sh
+++ wallaby/console/install.sh
@@ -3,8 +3,8 @@
 set -x
 set -e
 ssh openstack@controller sudo bash os_inst/wallaby/controller/install.sh
-ssh openstack@storage1 sudo bash os_inst/wallaby/block1/install.sh
-ssh openstack@storage2 sudo bash os_inst/wallaby/block2/install.sh
+ssh openstack@storage1 sudo bash os_inst/wallaby/storage1/install.sh
+ssh openstack@storage2 sudo bash os_inst/wallaby/storage2/install.sh
 ssh openstack@compute1 sudo bash os_inst/wallaby/compute1/install.sh
 ssh openstack@compute2 sudo bash os_inst/wallaby/compute2/install.sh
 ssh openstack@controller sudo bash os_inst/wallaby/controller/register-computes.sh
