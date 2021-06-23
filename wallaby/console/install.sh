7e6e54e155a4daa09db2d1f0d27a57d7862a806d
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-06-23 08:08:41 +0200

    Console: OS client installation incorporated into 'install.sh'

--- wallaby/console/install.sh
+++ wallaby/console/install.sh
@@ -2,6 +2,17 @@
 
 set -x
 set -e
+echo "---> Installing OpenStack Client"
+sudo apt install -y python3-openstackclient > /dev/null
+cat << EOF > admin-openrc
+export OS_USERNAME=admin
+export OS_PASSWORD=openstack
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+EOF
 ssh openstack@controller sudo bash os_inst/wallaby/controller/install.sh
 ssh openstack@storage1 sudo bash os_inst/wallaby/storage1/install.sh
 ssh openstack@storage2 sudo bash os_inst/wallaby/storage2/install.sh
