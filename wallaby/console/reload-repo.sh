e5da256cae4263b8abda122aae7114bb157fe69f
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-06-23 07:30:53 +0200

    Update reload-repo.sh

--- wallaby/console/reload-repo.sh
+++ wallaby/console/reload-repo.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-for node in "controller compute1 compute2 storage1 storage2"
+set -x
+set -e
+
+for node in "controller" "compute1" "compute2" "storage1" "storage2"
 do
+  echo "---> reloding os_inst repo on $node"
   ssh openstack@$node rm -rf os_inst
   ssh openstack@$node git clone https://github.com/kriscelmer/os_inst
 done
+echo "--> reloading os_inst repo on console"
 rm -rf os_inst
 git clone https://github.com/kriscelmer/os_inst
