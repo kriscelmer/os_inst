79db8c0f0f87afe1b20a727e055499eb10790375
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-06-30 18:43:02 +0200

    Update 1-networking.sh

--- wallaby/console/1-networking.sh
+++ wallaby/console/1-networking.sh
@@ -48,4 +48,4 @@ sudo sh -c 'echo "DNS=8.8.8.8 8.8.4.4" >> /etc/systemd/resolved.conf'
 sudo systemctl restart systemd-resolved
 set +x
 echo "---> Networking configured"
-reboot
+echo "---> Reboot all machines!
