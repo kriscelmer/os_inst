078e6782ebd3f24e1c1fbb245a90a6156753f999
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-07-20 13:49:18 +0200

    Update install.sh

--- wallaby/compute1/install.sh
+++ wallaby/compute1/install.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -y > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get update -y > /dev/null
 for script in os_inst/wallaby/compute1/[2-4]*
 do
   bash $script
