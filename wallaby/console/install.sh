1460bf422db14f67ea8c5cb5c5988d8e4dd481ca
Kris Celmer <krzysztof_celmer@hotmail.com>
2021-08-10 19:21:44 +0200

    Update install.sh

--- wallaby/console/install.sh
+++ wallaby/console/install.sh
@@ -35,7 +35,7 @@ done
 #ssh openstack@storage2 sudo bash os_inst/wallaby/storage2/install.sh
 #ssh openstack@compute1 sudo bash os_inst/wallaby/compute1/install.sh
 #ssh openstack@compute2 sudo bash os_inst/wallaby/compute2/install.sh
-#ssh openstack@controller sudo bash os_inst/wallaby/controller/register-computes.sh
+ssh openstack@controller sudo bash os_inst/wallaby/controller/register-computes.sh
 
 # Prepare 'demo' account to course exercises
 . demo-openrc
