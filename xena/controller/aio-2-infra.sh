cd67b00f03a20c06ac90e12134ec9f3d98d47f7e
Kris Celmer <krzysztof_celmer@hotmail.com>
2022-01-07 20:21:58 +0100

    Update aio-2-infra.sh

--- xena/controller/aio-2-infra.sh
+++ xena/controller/aio-2-infra.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 cat << EOF | sfdisk /dev/sdb
 /dev/sdb1: size=60GB, type=8e
-/dev/sdb2: size=40GBm type=8e
+/dev/sdb2: size=40GB, type=8e
 /dev/sdb3: size=10GB, type=83
 /dev/sdb4: size=10GB, type=83
 write
