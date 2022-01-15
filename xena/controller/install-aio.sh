cbda36b3696702614d043dc2f49449e5f120e3fd
Kris Celmer <krzysztof_celmer@hotmail.com>
2022-01-15 15:03:06 +0100

    Aio clouds.yaml and demo account setup

--- xena/controller/install-aio.sh
+++ xena/controller/install-aio.sh
@@ -16,3 +16,30 @@ do
 done
 bash os_inst/xena/controller/configure-openstack.sh
 bash os_inst/xena/controller/register-computes.sh
+
+# Prepare 'demo' account to course exercises
+cat << EOF > demo-openrc
+export OS_USERNAME=demo
+export OS_PASSWORD=openstack
+export OS_PROJECT_NAME=demo
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+EOF
+. demo-openrc
+openstack keypair create --public-key /home/openstack/.ssh/id_rsa.pub demo-keypair
+openstack security group create demo-icmp-sg --description "Allow ICMP packets from any address"
+openstack security group rule create --remote-group demo-icmp-sg --ingress --ethertype IPv4 demo-icmp-sg
+openstack security group rule create --remote-ip 0.0.0.0/0 --protocol icmp --ingress --ethertype IPv4 demo-icmp-sg
+
+# Copy example files to /home/openstack/examples
+mkdir -p /home/openstack/examples
+cp -r os_inst/xena/controller/examples/* /home/openstack/examples
+chown -R openstack /home/openstack/examples
+
+# Copy clouds.yaml for /home/openstack/.config/openstack
+mkdir -p /home/openstack/.config/openstack
+cp os_inst/xena/controller/clouds.yaml /home/openstack/.config/openstack
+chown openstack /home/openstack/.config
+chown -R /home/openstack/.config/openstack
