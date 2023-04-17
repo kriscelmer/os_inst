83e426660e90a562da21795690991bf27fdcce9e
Kris Celmer <krzysztof_celmer@hotmail.com>
2023-04-17 14:38:52 +0200

    Ussuri corrections

--- ussuri/controller/aio-6-nova.sh
+++ ussuri/controller/aio-6-nova.sh
@@ -6,6 +6,10 @@ set -x
 DEBIAN_FRONTEND=noninteractive apt-get install -y nova-compute > /dev/null
 crudini --set /etc/nova/nova.conf cinder os_region_name RegionOne
 crudini --set /etc/nova/nova-compute.conf libvirt virt_type qemu
+crudini --set /etc/nova/nova.conf vnc enabled true
+crudini --set /etc/nova/nova.conf vnc server_listen 0.0.0.0
+crudini --set /etc/nova/nova.conf vnc server_proxyclient_address '$my_ip'
+crudini --set /etc/nova/nova.conf vnc novncproxy_base_url http://10.0.0.11:6080/vnc_auto.html
 service nova-compute restart
 set +x
-echo "---> Nova installed on compute1"
+echo "---> Nova installed on controller"
--- ussuri/controller/install-aio.sh
+++ ussuri/controller/install-aio.sh
@@ -4,17 +4,18 @@ set -x
 set -e
 
 DEBIAN_FRONTEND=noninteractive apt-get update -y > /dev/null
-for script in os_inst/wallaby/controller/[2-9]-*
+for script in os_inst/ussuri/controller/[2-9]-*
 do
   bash $script
 done
-bash os_inst/wallaby/controller/10-heat.sh
-bash os_inst/wallaby/controller/19-horizon.sh
-for script in os_inst/wallaby/controller/aio-[2-9]-*
+bash os_inst/ussuri/controller/10-heat.sh
+bash os_inst/ussuri/controller/19-horizon.sh
+for script in os_inst/ussuri/controller/aio-[2-9]-*
 do
   bash $script
 done
-bash os_inst/wallaby/controller/configure-openstack.sh
+bash os_inst/ussuri/controller/configure-openstack.sh
+bash os_inst/ussuri/controller/register-computes.sh
 
 # Prepare 'demo' account to course exercises
 cat << EOF > demo-openrc
