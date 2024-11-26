644ef13b0c75f32ab4461d4dc937dcfb638c12bf
Kris Celmer <krzysztof_celmer@hotmail.com>
2024-11-26 11:00:46 +0100

    Nova fix for Caracal

--- caracal/controller/5-placement.sh
+++ caracal/controller/5-placement.sh
@@ -29,5 +29,6 @@ crudini --set /etc/placement/placement.conf keystone_authtoken password openstac
 su -s /bin/sh -c "placement-manage db sync" placement
 service apache2 restart
 openstack --os-placement-api-version 1.2 resource class list --sort-column name
+openstack --os-placement-api-version 1.6 trait list --sort-column name
 set +x
 echo "---> Placement installed"
